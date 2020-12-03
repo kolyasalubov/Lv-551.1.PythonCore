ef266bcf0a0e93df49a57b409f66dfc21f197052
Lekuro <lekurot@gmail.com>
2020-12-03 05:59:59 +0200

    added first HomeWork

---
+++ hw1.py
@@ -0,0 +1,10 @@
+num1 = float(input('Pleuse enter a number: '))
+num2 = float(input('Enter a number one more time: '))
+
+print('num1 + num2 =', num1 + num2)
+print('num1 - num2 =', num1 - num2)
+print('num1 * num2 =', num1 * num2)
+print('num1 ** num2 =', num1 ** num2)
+print('num1 / num2 =', num1 / num2)
+print('num1 // num2 =', num1 // num2)
+print('num1 % num2 =', num1 % num2)
